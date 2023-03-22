fdac52592e6e776585a7812c26dbd4f131d713a3
Pr1ntel <vandushev.sergey@yandex.ru>
2023-03-23 01:11:09 +0300

    add bootstrap,rename files, made templates

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import React,{useEffect} from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+const App = () => {
+
+    return (
+//TODO шаблон
+<div>
+    <button className={"btn btn-primary"}>Press pls</button>
+</div>
+    );
+}
+export default App;
