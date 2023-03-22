f0fea3c5f0375fcb983d8c5d292dc8fb0290c911
Pr1ntel <vandushev.sergey@yandex.ru>
2023-03-22 17:45:19 +0300

    create git repository from site

---
+++ src/main/java/com/example/filmsbysergoandalex/FilmsBySergoAndAlexApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmsbysergoandalex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsBySergoAndAlexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmsBySergoAndAlexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/filmsbysergoandalex/FilmsBySergoAndAlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmsbysergoandalex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsBySergoAndAlexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
